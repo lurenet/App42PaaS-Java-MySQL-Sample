a3a1fc88e6d44fce9618e49d322b6391273bdfdf
nickmas <nikolay.maslianikov@gmail.com>
2021-04-27 09:55:55 +0300

    debug-11

--- src/com/shephertz/app42/paas/sample/FormSave.java
+++ src/com/shephertz/app42/paas/sample/FormSave.java
@@ -49,13 +49,15 @@ public class FormSave extends HttpServlet {
 		String query = "INSERT INTO user(name,description,email) VALUES('"
 					+ name + "', '" + description + "', '" + email + "')";
 
+		DBManager db = new DBManager();
+
 		try {
 			// insert query
 			// String query = "INSERT INTO user(name,description,email) VALUES('"
 			// 		+ name + "', '" + description + "', '" + email + "')";
 			System.out.println("Query: " + query);
 			// Database Manager called
-			DBManager db = new DBManager();
+			// DBManager db = new DBManager();
 			db.insert(query);
 			// Redirect to new url
 		String newUrl = "http://" + request.getServerName() + ":"
@@ -68,6 +70,15 @@ public class FormSave extends HttpServlet {
 			PrintWriter out = response.getWriter();
 			out.print("<!doctype html><html><head><meta charset='utf-8'><title>App42 Sample Java-MySql Application</title><link href='css/style-User-Input-Form.css' rel='stylesheet' type='text/css'></head><body><div class='App42PaaS_header_wrapper'><div class='App42PaaS_header_inner'><div class='App42PaaS_header'><div class='logo'><a href='http://app42paas.shephertz.com'><img border='0' alt='App42PaaS' src='images/logo.png'></img></a></div></div></div></div><div class='App42PaaS_body_wrapper'><div class='App42PaaS_body'><div class='App42PaaS_body_inner'><div class='contactPage_title'>");
 			out.print("<h2 align='center'>3-Error occured. See Logs.</h2><br/><br/>");
+			// debug
+			out.print("<div>Query: "+query+"</div><br/>");
+			out.print("<div>DB: "+db!=NULL+"</div><br/>");
+			out.print("<div>DBip: "+Util.getDBIp()+"</div><br/>");
+			out.print("<div>DBname: "+Util.getDBName()+"</div><br/>");
+			out.print("<div>DBuser: "+Util.getDBUser()+"</div><br/>");
+			out.print("<div>DBpasswd: "+Util.getDBPassword()+"</div><br/>");
+			out.print("<div>DBpasswd: "+Util.getDBPassword()+"</div><br/>");
+			// debug
 			out.print("<br/><a href='/' style='font-size: 18px;'>Back</a>");
 			out.print("</div></div></div></div></body></html>");
 		}
